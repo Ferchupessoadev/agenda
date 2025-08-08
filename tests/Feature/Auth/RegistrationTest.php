9632c315353c16379115b589c59a5742f41f0a25
Ferchupessoadev <fernandomatiaspessoa471@gmail.com>
2025-08-08 00:12:07 -0300

    add: DatabaseSeeder in the Test 'RegistrationTest'

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -9,6 +9,8 @@ test('registration screen can be rendered', function () {
 });
 
 test('new users can register', function () {
+    $this->seed();
+
     $response = $this->post('/register', [
         'name' => 'Test User',
         'email' => 'test@example.com',
